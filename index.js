6c197f9d37acd40202876cb3d9a5887b5d7e8000
Natan Loterio <natanloterio@gmail.com>
2020-04-14 13:09:06 -0300

    added desktop notifications

--- index.js
+++ index.js
@@ -2,12 +2,28 @@
 //var timeReg = /d-t=*\d*\s/g;
 const yargs = require('yargs');
 const player = require('play-sound')(opts = {})
+const notifier = require('node-notifier');
+const path = require('path');	
+const colors = require('colors');
+const reg = {id:0}
 
 function alarm(){
     player.play('finish.mp3',function(err){
         if (err) throw err
       });
       console.log("Well Done!")
+      notifier.notify(
+        {
+          title: 'Pomogo',
+          message: 'Well done! Now take a rest!',
+          icon: path.join(__dirname, 'pomodoro.png'), // Absolute path (doesn't work on balloons)
+          sound: true, // Only Notification Center or Windows Toasters
+          wait: true // Wait with callback, until user action is taken against notification, does not apply to Windows Toasters as they always wait or notify-send as it does not support the wait option
+        },
+        function(err, response) {
+          // Response is response from notification
+        }
+      );
 }
 
 function startTimer(timeout){
@@ -17,7 +33,7 @@ function startTimer(timeout){
       });    
     console.log("starting now:"+now)
     console.log("work until:"+new Date(now.getTime()+timeout))
-    setTimeout(alarm,timeout)
+    reg.id = setTimeout(alarm,timeout)
 }
 
 const argv = yargs
@@ -44,4 +60,15 @@ if (argv.minutes) {
     duration = argv.minutes;
 }
 
+process.on('SIGINT', function() {
+    console.log("\nAborting timer");
+    clearTimeout(reg.id)
+    process.exit();
+});
+
+
 startTimer(duration*60*1000);
+
+
+
+console.log("%cThis is a %cConsole.log", "background:black ; color: white", "color: red; font-size:25px");
